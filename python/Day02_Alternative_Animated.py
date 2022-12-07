c9ae2bb7274d278406743eb8a2bd52a2e1cc8ac2
Dr. Markus Weninger <markus.weninger@jku.at>
2022-12-07 07:51:15 +0100

    Day 2: Visualization

--- python/Day02_Alternative_Animated.py
+++ python/Day02_Alternative_Animated.py
@@ -1,5 +1,6 @@
-import Helper
+import dataclasses
 import matplotlib.pyplot as plt
+import Helper
 from celluloid import Camera
 
 
@@ -71,58 +72,90 @@ class Animated:
 
     def animate(self, *args, **kwargs): return self.camera.animate(*args, **kwargs)
 
+# Left: A for Rock, B for Paper, and C for Scissors
+# Right: X for Rock, Y for Paper, and Z for Scissors
+
+# Shape score: 1 for Rock, 2 for Paper, and 3 for Scissors
+# Result score: 0 if you lost, 3 if the round was a draw, and 6 if you won
+
+
+A = "A"
+B = "B"
+C = "C"
+X = "X"
+Y = "Y"
+Z = "Z"
+R = "R"
+P = "P"
+S = "S"
+
+DRAW = "DRAW"
+LOSE = "LOSE"
+WIN = "WIN"
+
+M = {
+    (R, R): 3+1, (R, P): 6+2, (R, S): 0+3,
+    (P, R): 0+1, (P, P): 3+2, (P, S): 6+3,
+    (S, R): 6+1, (S, P): 0+2, (S, S): 3+3,
+}
+
+M2 = {
+    (R, LOSE): 0+3, (R, DRAW): 3+1, (R, WIN): 6+2,
+    (P, LOSE): 0+1, (P, DRAW): 3+2, (P, WIN): 6+3,
+    (S, LOSE): 0+2, (S, DRAW): 3+3, (S, WIN): 6+1,
+}
+
+# X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+
 
 def star01(test):
     data = Helper.readData(
-        day=1,
+        day=2,
         star=-1 if test else 1,
-        line_converter=lambda line: int(line) if line != '' else '',
-        list_converter=lambda lines: [sum(group) for group in Helper.empty_line_seperated_groups(lines)]
+        text_converter=lambda text: text.replace(A, R).replace(X, R).replace(B, P).replace(Y, P).replace(C, S).replace(Z, S),
+        line_converter=lambda line: M[tuple(line.split())]
     )
-    maximum = 0
 
     animation = Animated()
-    step_size = 1 if test else 3
+    step_size = 1 if test else 50
     for frame in [*range(1, len(data), step_size)] + [len(data)]:
         x = range(0, frame)
         y = data[0:frame]
         animation.set_x(x)
         animation.set_y(y)
 
-        maximum = max(y)
-        animation.set_highlights([maximum], ["Maximum"])
+        animation.set_highlights([], [])
         animation.render()
+        print(f"Frame {frame} of {len(data)}")
 
-    animation.animate(interval=500 if test else 10).save(f"./Animations/Day01_Star01{'test' if test else ''}.gif")
+    animation.animate(interval=500 if test else 10).save(f"./Animations/Day02_Star01{'test' if test else ''}.gif")
 
-    print('maximum:', maximum)
+    print(sum(data))
 
 
 def star02(test):
     data = Helper.readData(
-        day=1,
+        day=2,
         star=-2 if test else 2,
-        line_converter=lambda line: int(line) if line != '' else '',
-        list_converter=lambda lines: [sum(group) for group in Helper.empty_line_seperated_groups(lines)]
+        text_converter=lambda text: text.replace(A, R).replace(B, P).replace(C, S).replace(X, LOSE).replace(Y, DRAW).replace(Z, WIN),
+        line_converter=lambda line: M2[tuple(line.split())]
     )
-    sumTopThree = 0
 
     animation = Animated()
-    step_size = 1 if test else 3
+    step_size = 1 if test else 50
     for frame in [*range(1, len(data), step_size)] + [len(data)]:
         x = range(0, frame)
         y = data[0:frame]
         animation.set_x(x)
         animation.set_y(y)
 
-        maximum_heights = sorted(y, reverse=True)[0:3]
-        sumTopThree = sum(maximum_heights)
-        animation.set_highlights(maximum_heights, ["1st", "2nd", "3rd"])
+        animation.set_highlights([], [])
         animation.render()
+        print(f"Frame {frame} of {len(data)}")
 
-    animation.animate(interval=500 if test else 10).save(f"./Animations/Day01_Star02{'test' if test else ''}.gif")
+    animation.animate(interval=500 if test else 10).save(f"./Animations/Day02_Star02{'test' if test else ''}.gif")
 
-    print('sumTopThree:', sumTopThree)
+    print(sum(data))
 
 
 star01(True)
